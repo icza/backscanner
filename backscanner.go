c3468666048fd51c57cf66f9b5e627d7d2806c67
Andras Belicza <iczaaa@gmail.com>
2017-10-18 10:04:18 +0200

    Added go file with package doc. Improved README, added badges. Added travis config.

---
+++ backscanner.go
@@ -0,0 +1,6 @@
+/*
+Package backscanner provides a scanner similar to bufio.Scanner that reads
+and returns lines in reverse order, starting at a given position (which may be
+the end of the input).
+*/
+package backscanner
